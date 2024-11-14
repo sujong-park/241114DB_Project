0eb92ff1278e199663aa027e2da230dd363aeb0c
alex6 <alex9305@naver.com>
2024-11-14 13:57:47 +0900

    수정

--- DB_241114/src/dao/CreateTable.java
+++ DB_241114/src/dao/CreateTable.java
@@ -7,15 +7,19 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class CreateTable {
+    private static final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String userid = "scott";
+    private static final String passwd = "tiger";
     public static ArrayList<String> CreateTables = new ArrayList<>();
+    public static ArrayList<String> DeleteTables = new ArrayList<>();
 
     static {
         // 순서를 보장하여 CreateTables를 채우기
         CreateTables.clear();
 
-// 1. 사용자 테이블 생성
+        // 1. 사용자 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRUSERTABLE (
+                    CREATE TABLE KUSERTABLE (
                         USERNO NUMBER PRIMARY KEY,
                         USERID VARCHAR2(20) UNIQUE NOT NULL,
                         USERPASSWORD VARCHAR2(20) NOT NULL,
@@ -23,184 +27,219 @@ public class CreateTable {
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRUSERNO_SEQ
+                    CREATE SEQUENCE KUSERNO_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRUSERTABLE_TRG
-                    BEFORE INSERT ON DDRUSERTABLE
+                    CREATE OR REPLACE TRIGGER KUSERTABLE_TRG
+                    BEFORE INSERT ON KUSERTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.USERNO := DDRUSERNO_SEQ.NEXTVAL;
+                        :NEW.USERNO := KUSERNO_SEQ.NEXTVAL;
                     END;
                 """);
 
-// 2. 작가, 출판사, 장르 테이블 생성
+        // 2. 작가, 출판사, 장르 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRAUTHORTABLE(
+                    CREATE TABLE KAUTHORTABLE(
                         AUTHORNO NUMBER PRIMARY KEY,
                         AUTHORNAME VARCHAR2(20) NOT NULL
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRAUTHORNO_SEQ
+                    CREATE SEQUENCE KAUTHORNO_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRAUTHORTABLE_TRG
-                    BEFORE INSERT ON DDRAUTHORTABLE
+                    CREATE OR REPLACE TRIGGER KAUTHORTABLE_TRG
+                    BEFORE INSERT ON KAUTHORTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.AUTHORNO := DDRAUTHORNO_SEQ.NEXTVAL;
+                        :NEW.AUTHORNO := KAUTHORNO_SEQ.NEXTVAL;
                     END;
                 """);
 
         CreateTables.add("""
-                    CREATE TABLE DDRPUBLISHERTABLE(
+                    CREATE TABLE KPUBLISHERTABLE(
                         PUBLISHERNO NUMBER PRIMARY KEY,
                         PUBLISHERNAME VARCHAR2(20) NOT NULL
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRPUBLISHER_SEQ
+                    CREATE SEQUENCE KPUBLISHER_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRPUBLISHERTABLE_TRG
-                    BEFORE INSERT ON DDRPUBLISHERTABLE
+                    CREATE OR REPLACE TRIGGER KPUBLISHERTABLE_TRG
+                    BEFORE INSERT ON KPUBLISHERTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.PUBLISHERNO := DDRPUBLISHER_SEQ.NEXTVAL;
+                        :NEW.PUBLISHERNO := KPUBLISHER_SEQ.NEXTVAL;
                     END;
                 """);
 
         CreateTables.add("""
-                    CREATE TABLE DDRGENRETABLE(
+                    CREATE TABLE KGENRETABLE(
                         GENRETABLENO NUMBER PRIMARY KEY,
                         GENRETABLENAME VARCHAR2(20) NOT NULL
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRGENRETABLE_SEQ
+                    CREATE SEQUENCE KGENRETABLE_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRGENRETABLE_TRG
-                    BEFORE INSERT ON DDRGENRETABLE
+                    CREATE OR REPLACE TRIGGER KGENRETABLE_TRG
+                    BEFORE INSERT ON KGENRETABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.GENRETABLENO := DDRGENRETABLE_SEQ.NEXTVAL;
+                        :NEW.GENRETABLENO := KGENRETABLE_SEQ.NEXTVAL;
                     END;
                 """);
 
-// 3. 책 테이블 생성
+        // 3. 책 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRBOOKTABLE(
+                    CREATE TABLE KBOOKTABLE(
                         BOOKNO NUMBER PRIMARY KEY,
                         BOOKNAME VARCHAR2(20) NOT NULL,
                         AUTHORNO NUMBER,
                         PUBLISHERNO NUMBER,
-                        FOREIGN KEY (AUTHORNO) REFERENCES DDRAUTHORTABLE(AUTHORNO),
-                        FOREIGN KEY (PUBLISHERNO) REFERENCES DDRPUBLISHERTABLE(PUBLISHERNO),
+                        FOREIGN KEY (AUTHORNO) REFERENCES KAUTHORTABLE(AUTHORNO),
+                        FOREIGN KEY (PUBLISHERNO) REFERENCES KPUBLISHERTABLE(PUBLISHERNO),
                         COUNT NUMBER
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRBOOKNO_SEQ
+                    CREATE SEQUENCE KBOOKNO_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRBOOKTABLE_TRG
-                    BEFORE INSERT ON DDRBOOKTABLE
+                    CREATE OR REPLACE TRIGGER KBOOKTABLE_TRG
+                    BEFORE INSERT ON KBOOKTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.BOOKNO := DDRBOOKNO_SEQ.NEXTVAL;
+                        :NEW.BOOKNO := KBOOKNO_SEQ.NEXTVAL;
                     END;
                 """);
 
-// 4. 장르-책 매핑 테이블 생성
+        // 4. 장르-책 매핑 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRGENREBOOKTABLE(
+                    CREATE TABLE KGENREBOOKTABLE(
                         GENRETABLENO NUMBER,
                         BOOKNO NUMBER,
                         PRIMARY KEY (GENRETABLENO, BOOKNO),
-                        FOREIGN KEY (GENRETABLENO) REFERENCES DDRGENRETABLE(GENRETABLENO),
-                        FOREIGN KEY (BOOKNO) REFERENCES DDRBOOKTABLE(BOOKNO)
+                        FOREIGN KEY (GENRETABLENO) REFERENCES KGENRETABLE(GENRETABLENO),
+                        FOREIGN KEY (BOOKNO) REFERENCES KBOOKTABLE(BOOKNO)
                     )
                 """);
 
-// 5. 랜탈 테이블 생성
+        // 5. 랜탈 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRRENTALTABLE (
+                    CREATE TABLE KRENTALTABLE (
                         RENTALNO NUMBER PRIMARY KEY,
                         BOOKNO NUMBER,
                         USERNO NUMBER,
                         RENTALSTARTDATE DATE DEFAULT SYSDATE,
                         RENTALENDDATE DATE DEFAULT SYSDATE + 7,
-                        FOREIGN KEY (BOOKNO) REFERENCES DDRBOOKTABLE(BOOKNO),
-                        FOREIGN KEY (USERNO) REFERENCES DDRUSERTABLE(USERNO)
+                        FOREIGN KEY (BOOKNO) REFERENCES KBOOKTABLE(BOOKNO),
+                        FOREIGN KEY (USERNO) REFERENCES KUSERTABLE(USERNO)
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRRENTALTABLE_SEQ
+                    CREATE SEQUENCE KRENTALTABLE_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRRENTALTABLE_TRG
-                    BEFORE INSERT ON DDRRENTALTABLE
+                    CREATE OR REPLACE TRIGGER KRENTALTABLE_TRG
+                    BEFORE INSERT ON KRENTALTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.RENTALNO := DDRRENTALTABLE_SEQ.NEXTVAL;
+                        :NEW.RENTALNO := KRENTALTABLE_SEQ.NEXTVAL;
                     END;
                 """);
 
-// 6. 장바구니 테이블 생성
+        // 6. 장바구니 테이블 생성
         CreateTables.add("""
-                    CREATE TABLE DDRSHSHOPPINGCARTTABLE (
+                    CREATE TABLE KSHOPPINGCARTTABLE (
                         SHOPPINGCARTNO NUMBER PRIMARY KEY,
                         BOOKNO NUMBER,
                         USERNO NUMBER,
-                        FOREIGN KEY (BOOKNO) REFERENCES DDRBOOKTABLE(BOOKNO),
-                        FOREIGN KEY (USERNO) REFERENCES DDRUSERTABLE(USERNO)
+                        FOREIGN KEY (BOOKNO) REFERENCES KBOOKTABLE(BOOKNO),
+                        FOREIGN KEY (USERNO) REFERENCES KUSERTABLE(USERNO)
                     )
                 """);
         CreateTables.add("""
-                    CREATE SEQUENCE DDRSHSHOPPINGCART_SEQ
+                    CREATE SEQUENCE KSHOPPINGCART_SEQ
                     START WITH 1
                     INCREMENT BY 1
                     NOCACHE
                     NOCYCLE
                 """);
         CreateTables.add("""
-                    CREATE OR REPLACE TRIGGER DDRSHSHOPPINGCARTTABLE_TRG
-                    BEFORE INSERT ON DDRSHSHOPPINGCARTTABLE
+                    CREATE OR REPLACE TRIGGER KSHOPPINGCARTTABLE_TRG
+                    BEFORE INSERT ON KSHOPPINGCARTTABLE
                     FOR EACH ROW
                     BEGIN
-                        :NEW.SHOPPINGCARTNO := DDRSHSHOPPINGCART_SEQ.NEXTVAL;
+                        :NEW.SHOPPINGCARTNO := KSHOPPINGCART_SEQ.NEXTVAL;
                     END;
                 """);
 
+        // 1. 트리거 삭제
+        DeleteTables.add("DROP TRIGGER KUSERTABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KAUTHORTABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KPUBLISHERTABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KGENRETABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KBOOKTABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KRENTALTABLE_TRG");
+        DeleteTables.add("DROP TRIGGER KSHOPPINGCARTTABLE_TRG");
+
+        // 2. 자식 테이블 삭제
+        DeleteTables.add("DROP TABLE KGENREBOOKTABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KSHOPPINGCARTTABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KRENTALTABLE CASCADE CONSTRAINTS");
+
+        // 3. 부모 테이블 삭제
+        DeleteTables.add("DROP TABLE KBOOKTABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KGENRETABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KAUTHORTABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KPUBLISHERTABLE CASCADE CONSTRAINTS");
+        DeleteTables.add("DROP TABLE KUSERTABLE CASCADE CONSTRAINTS");
+
+        // 4. 시퀀스 삭제
+        DeleteTables.add("DROP SEQUENCE KUSERNO_SEQ");
+        DeleteTables.add("DROP SEQUENCE KAUTHORNO_SEQ");
+        DeleteTables.add("DROP SEQUENCE KPUBLISHER_SEQ");
+        DeleteTables.add("DROP SEQUENCE KGENRETABLE_SEQ");
+        DeleteTables.add("DROP SEQUENCE KBOOKNO_SEQ");
+        DeleteTables.add("DROP SEQUENCE KRENTALTABLE_SEQ");
+        DeleteTables.add("DROP SEQUENCE KSHOPPINGCART_SEQ");
     }
 
-    public static boolean executeUpdateSql(String sql, String url, String userid, String passwd) {
+    public static void deleteTable(){
+        for(String sql: DeleteTables){
+            executeUpdateSql(sql);
+        }
+    }
+
+    public static boolean executeUpdateSql(String sql) {
         try (Connection con = DriverManager.getConnection(url, userid, passwd);
              Statement stmt = con.createStatement()) {
 
@@ -216,11 +255,10 @@ public class CreateTable {
     }
 
     public static void main(String[] args) {
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        String userid = "scott";
-        String passwd = "tiger";
         for (String sql : CreateTables) {
-            executeUpdateSql(sql, url, userid, passwd);
+            executeUpdateSql(sql);
         }
+//      해당 주석처리한 코드 실행시 모든 테이블, 트리거, 시퀀스 삭제.
+//      deleteTable();
     }
 }
